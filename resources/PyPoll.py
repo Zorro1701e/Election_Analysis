1d80fa428c7802cae7d181186ea87abfe588f8d7
Zorro1701e <98936157+Zorro1701e@users.noreply.github.com>
2022-03-13 15:37:09 -0700

    Adding the election analysis.

--- resources/PyPoll.py
+++ resources/PyPoll.py
@@ -1,5 +1,5 @@
 # The DATA we need to retrieve.
-    # 1. The total number of votes cast.
+    # 1. The total number of votes cast. (369,711)
     # 2. A complete lists of Candidates who recieved votes.
     # 3. Percentage of votes each candidate won.
     # 4. Total number of votes each candidate won. 
@@ -18,13 +18,42 @@ import os
 # Files to load and output (Remember to change these)
 file_to_load = os.path.join(".", "election_results.csv")
 file_to_save = os.path.join('.', 'election_analysis.txt')
-   
+
+# 1. Inititalize counter to Zero
+total_votes = 0
+
+#Candidate options 
+candidate_options = []
+# I DECLARE AN EMPTY DICTIONARY!
+candidate_votes = {}
+
+#open election results & read file
 with open(file_to_load) as election_data:
         file_reader = csv.reader(election_data)
 
         #Read the Header row
         headers = next(file_reader)
 
-        #Print each row in the CSV file.
+    #Print each row in the CSV file.
         for row in file_reader:
-            print(headers)
+            # 2. add to the total vote count..
+            total_votes += 1
+
+        #print the candidate name from each row.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate THEN...
+        if candidate_name not in candidate_options:
+            # ADD it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+            # begin tracking THAT candidates vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+
+# print the candidate vote dictionary
+print(candidate_votes)
+   
