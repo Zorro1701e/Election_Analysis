805dda8def1a247f74bffb36947c486d720a7112
Zorro1701e <98936157+Zorro1701e@users.noreply.github.com>
2022-04-01 11:58:31 -0700

    message

---
+++ week6/Vacation_Search/config.py
@@ -0,0 +1,4 @@
+weather_api_key="10cfcf54f98d9c2d3b16f3f5cb6cd93b"
+
+#google API Key
+g_key = "AIzaSyBw6ZIZOdAaQQw6owlEd7tkKdogwbzvybU"
---
+++ week6/config2.py
@@ -0,0 +1,2 @@
+#google API Key
+g_key = "AIzaSyBw6ZIZOdAaQQw6owlEd7tkKdogwbzvybU"
